002332ab656c260a72a61216a47dc5273b35862e
Jason Cummings <jasoncummings86@gmail.com>
2018-07-27 11:24:53 -0400

    use globals instead of threads before refactor

--- client.rb
+++ client.rb
@@ -4,7 +4,7 @@ require './stats'
 
 module AntMiner
   class Config
-    attr_accessor :addresses, :port, :writer, :ws, :reader
+    attr_accessor :port, :writer, :reader
   end
 
   class Client
@@ -12,31 +12,25 @@ module AntMiner
     attr_accessor :stats
 
     def initialize
-      @stats  = {}
       @config = Config.new
       yield @config
-      addresses.each { |a| stats[a] = [] }
-      reader.close
     end
 
     def monitor
       at_exit do
-        writer.close
         puts '*' * 30
         puts "\nClosing socket server..."
-        Process.kill 9, ws
         puts "\nClosing sockets..."
         puts "\nexiting..."
       end
 
-      puts '*' * 45
-      puts "Getting stats from #{addresses.count} miners\n"
-      puts '*' * 45
+      puts '=' * 45
+      puts "Getting stats from #{$addresses.count} miners\n"
 
       loop do
         sleep 1
 
-        addresses.each do |address|
+        $addresses.each do |address|
           begin
             socket = TCPSocket.open(address, port)
           rescue Errno::ETIMEDOUT
@@ -45,17 +39,14 @@ module AntMiner
           end
           json = Api::Stats.get_temps(socket)
           socket.close
-          stats[address] << json
-          writer.puts json
+          $stats[address] << json
           puts "Logged stats for #{address}"
         end
       end
     end
 
-    def addresses ; config.addresses end
     def port      ; config.port      end
     def writer    ; config.writer    end
-    def ws        ; config.ws        end
     def reader    ; config.reader    end
   end
 end
--- connection.rb
+++ connection.rb
@@ -1,23 +1,28 @@
-require "./client"
+require './client'
 require 'em-websocket'
 
-rd, wt = IO.pipe
+$addresses = File.read('addresses.txt').split(/\n/)
+$stats = {}
+$addresses.each { |a| $stats[a] = [] }
 
-pid = fork do
-  wt.close
-  EventMachine::WebSocket.start(host: "0.0.0.0", port: 8080) do |ws|
-    ws.onopen    { ws.send "yeehaw" }
-    ws.onmessage { |msg| ws.send "Pong: #{rd.gets}" }
-    ws.onclose   { puts "WebSocket closed" }
+thread1 = Thread.new do
+  EventMachine::WebSocket.start(host: '0.0.0.0', port: 8080) do |ws|
+    ws.onopen { ws.send 'yeehaw' }
+    ws.onmessage { |msg|
+      ip, _command = msg.split(/:/)
+      latest = $stats[ip].last
+      ws.send latest.to_json
+    }
+    ws.onclose { puts 'WebSocket closed' }
   end
 end
 
+at_exit do
+  Thread.kill thread1
+end
+
 client = AntMiner::Client.new do |config|
-  config.addresses = File.read('addresses.txt').split(/\n/)
   config.port = 4028
-  config.writer = wt
-  config.reader = rd
-  config.ws = pid
 end
 
 client.monitor
